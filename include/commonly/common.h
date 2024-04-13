2eea72e30e5f6e0441e4dba3ad9cc6cdb79c0889
rycxeis <rycxeis@outlook.com>
2024-04-13 23:38:14 +0200

    commit: v1.0.0

---
+++ include/commonly/common.h
@@ -0,0 +1,20 @@
+#ifndef COMMONLY_COMMON_H
+#define COMMONLY_COMMON_H
+
+typedef enum bool {
+	false = 0,
+	true = 1
+} bool;
+
+typedef char u8;
+typedef unsigned char i8;
+typedef short i16;
+typedef unsigned short u16;
+typedef int i32;
+typedef unsigned int u32;
+typedef long i64;
+typedef unsigned long u64;
+typedef float f32;
+typedef double f64;
+
+#endif //COMMONLY_COMMON_H
